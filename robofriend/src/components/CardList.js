81966041aec6f6f7e968bea7a4c7c852cd066c6b
ZeidMag <47781347+ZeidMag@users.noreply.github.com>
2019-04-03 11:57:38 +0200

    first commit

---
+++ robofriend/src/components/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+import './CardList.css';
+
+const CardList = ({ robots }) => {
+    if(false){
+        throw new Error('NOOOOOO');
+    }
+    return(
+    <div>
+        {
+        robots.map((user,i)=> {
+            return ( 
+                <Card 
+                key={i} 
+                id={robots[i].id} 
+                name={robots[i].name} 
+                email={robots[i].email}
+                />
+            );    
+        })
+    }
+    </div> 
+    )
+}
+
+export default CardList;
---
+++ robofriend/src/robots.js
@@ -0,0 +1,69 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'زيد المغبوب',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'سارة جرانة',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'هالة المغبوب',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'سارة حنيش',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'زياد محمد',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'محمود حنيش',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'طه جرانة',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'سمية المغبوب',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'سمية جرانة',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'المغبوب جرانة',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    },
+    {
+      id: 11,
+      name: 'الككلي بلعيد',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
